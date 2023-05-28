2ffc2e5d2679103bf8be525fa4e89a5d92def60b
Roshan Paste <121104650+RoshanPaste@users.noreply.github.com>
2023-05-28 22:10:17 +0530

    added var&let&const.js

---
+++ var&let&const.js
@@ -0,0 +1,19 @@
+// const pi=3.14;
+// var pi=4;
+// console.log(pi);
+
+
+function oddSum(n)
+{
+    let total = 0, result=[]; 
+    for(let x = 1; x <= n; x++) 
+    { 
+       let odd = 2*x-1; 
+       total += odd;
+       result.push(total);
+    }
+    return result;
+}
+
+var result = oddSum(5);
+console.log(result);
